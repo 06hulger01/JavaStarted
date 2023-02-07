d3b50897252ed8eb27df0f70fbdd38e83a1a148e
06hulger01 <haciulger1980@gmail.com>
2023-02-07 16:32:54 +0300

    first commit

---
+++ src/main/java/Exe.java
@@ -0,0 +1,87 @@
+public class Exe {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Haydi! Iam ready to be a java pro :))");
+
+        System.out.println(".......................");
+
+        System.out.println("1\n2\n3\n4\n5\n");
+
+        System.out.println(".......................");
+
+        System.out.println("\"hello world\"");
+
+        System.out.println(".......................");
+
+        System.out.println("21\" monitor");
+
+        System.out.println(".......................");
+
+        System.out.println("\'eve geç kalma\' dedi");
+
+        System.out.println(".......................");
+
+        System.out.println("  ************* ");
+        System.out.println("         **     ");
+        System.out.println("         **     ");
+        System.out.println("         **     ");
+        System.out.println("         **     ");
+        System.out.println(" **      **     ");
+        System.out.println("  **     **     ");
+        System.out.println("    **   **     ");
+        System.out.println("      ****      ");
+
+        System.out.println(".......................");
+
+        System.out.println("Ahmet\nGÜL\n30 yaşımdayım\nİstanbul da yaşıyorum");
+
+        System.out.println(".......................");
+
+
+        int i = 30;
+        int i2 = 35;
+        String st = "Ahmet";
+        String st1 = "Ayşe";
+        char c = 'M';
+        System.out.println("\"" + i2 + "," + st + "," + c + "\"");
+
+
+        System.out.println(".......................");
+
+        int bro1 = 15;
+        int bro2 = 15;
+        System.out.println(bro1 + bro2);
+        System.out.println(".......................");
+        int bro3 = 10, bro4 = bro3;
+        System.out.println("ikiz kardeşlerin yaşı 10");
+
+        System.out.println(".......................");
+
+        String lpg;
+        String motorin;
+        String gasoline;
+        lpg = "10";
+        motorin = "20";
+
+        String lpg1, motorin1, gasoline1;
+
+
+        System.out.println(".......................");
+
+        String str = "Fatih Sultan Mehmet";
+        String str2 = "yılında İstanbul u fethetmiştir.";
+        int in = 1453;
+        System.out.println(str + " " + in + " " + str2);
+
+
+        System.out.println(".......................");
+        double rx = 6;
+        double Pİ = 3.14;
+        double alan = rx * rx * Pİ;
+        double cevre = 2 * Pİ * rx;
+        System.out.println("alan= " + alan);
+        System.out.println("cevre= " + cevre);
+    }
+}
